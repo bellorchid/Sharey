83512eba2f94edac14d0bb448d4437514c789afc
birdgg <birdeggegg@gmail.com>
2014-11-14 18:26:37 +0800

    now can edit and delete

---
+++ client/views/posts/postEdit.js
@@ -0,0 +1,29 @@
+Template.PostEdit.helpers({
+    
+});
+
+Template.PostEdit.events({
+    'submit form': function(event, template){
+        event.preventDefault();
+        var currentPostId = this._id;
+
+        var postProperties = {
+            url: event.target.url.value,
+            title: event.target.title.value
+        };
+
+        Posts.update(currentPostId, {$set: postProperties},function(error){
+            if(error){ alert(error.reason); }
+            else { Router.go('home'); }
+        });
+    },
+
+    'click .delete': function(event, template){
+        event.preventDefault();
+        if (confirm("Delete this post?")) {
+          var currentPostId = this._id;
+          Posts.remove(currentPostId);
+          Router.go('home');
+        }
+    }
+});
