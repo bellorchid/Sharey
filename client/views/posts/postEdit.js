96f2c94adaf12ea231c3959df27218f124ecd7fe
birdgg <birdeggegg@gmail.com>
2014-11-19 21:31:19 +0800

    editvalidate

--- client/views/posts/postEdit.js
+++ client/views/posts/postEdit.js
@@ -1,18 +1,31 @@
-Template.PostEdit.helpers({
-    
+Template.postEdit.created = function() {
+    Session.set('postEditErrors', {});
+}
+
+Template.postEdit.helpers({
+    errorMsg: function(field){
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function(field){
+        return !!Session.get('postEditErrors')[field]?'has-error':'';
+    }
 });
 
-Template.PostEdit.events({
+Template.postEdit.events({
     'submit form': function(event, template){
         event.preventDefault();
         var currentPostId = this._id;
 
-        var postProperties = {
+        var post = {
             url: event.target.url.value,
             title: event.target.title.value
         };
+        var errors = validatePost(post);
+        if(errors.title || errors.url){
+            return Session.set('postEditErrors', errors);
+        }
 
-        Posts.update(currentPostId, {$set: postProperties},function(error){
+        Posts.update(currentPostId, {$set: post},function(error){
             if(error){
                 throwError(error.reason);
             }
