976879303f0768d1084aa3e7af3a24775fcdf231
birdgg <birdeggegg@gmail.com>
2014-12-10 19:18:07 +0800

    add active for navbar

---
+++ client/views/nav/nav.js
@@ -0,0 +1,12 @@
+Template.nav.helpers({
+  activeRouteClass: function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.pop();
+
+    var active = _.any(args, function(name) {
+      return Router.current() && Router.current().route.getName() === name
+    });
+
+    return active && 'active';
+  }
+});
