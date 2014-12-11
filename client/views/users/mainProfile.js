21f80f457299cb496d38cf593bfec597067abacd
birdgg <birdeggegg@gmail.com>
2014-12-12 00:07:50 +0800

    grab avatar from github

--- client/views/users/mainProfile.js
+++ client/views/users/mainProfile.js
@@ -1,5 +1,14 @@
 Template.mainProfile.helpers({
+    avatar: function(name) {
+        return 'https://avatars.githubusercontent.com/'+name;
+    },
     email: function() {
         return this.emails[0].address;
+    }
+});
+
+Template.mainProfile.events({
+    'click .profile-edit': function(e, p) {
+        Router.go('user.edit', {_id: this._id});
     }
 });
--- server/users.js
+++ server/users.js
@@ -1,4 +1,9 @@
 Accounts.onCreateUser(function(options, user){
-    user.profile = { postCount: 0 };
+    user.profile = {
+        postCount: 0,
+        bio: '',
+        website: '',
+        github: ''
+    };
     return user;
 });
